54b3285c7382e057c330d30c45efb78b47cef713
Asel Zhunushova <zhunushova29@gmail.com>
2025-02-12 00:37:05 +0600

    fix bugs

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -14,6 +15,7 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      'react': react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -23,6 +25,27 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "react/jsx-no-target-blank": "off", // Разрешаем target="_blank"
+      "react/prop-types": "off", // PropTypes не нужен в TS
+      "no-console": ["warn", { allow: ["warn", "error"] }], // Запрещаем console.log()
+      quotes: ["error", "single"], // Одинарные кавычки
+      "no-unused-vars": "off", // Отключаем стандартное правило
+      "@typescript-eslint/no-unused-vars": ["error"], // Предупреждаем о неиспользуемых переменных
+      semi: ["error", "never"], // Без точки с запятой
+      "no-var": ["error"], // Запрещаем var
+      "no-restricted-syntax": [
+        "warn",
+        {
+          selector: "MemberExpression[property.name='log']",
+          message: "Убери - console.log()",
+        },
+      ],
+      "jsx-quotes": ["error", "prefer-single"],
+      'react/jsx-indent': ['error', 2], // Отступ 2 пробела
+      'react/jsx-indent-props': ['error', 2], // Отступ 2 пробела для атрибутов
+      "react/jsx-max-props-per-line": ["warn", { "maximum": 1, "when":"always" }], 
+      "react/jsx-first-prop-new-line": ["warn", "multiline"],
+      "react/jsx-closing-bracket-location": ["warn", "tag-aligned"], // Закрывающая `>` на новой строке
     },
   },
 )
---
+++ src/Test.tsx
@@ -0,0 +1,13 @@
+export const Test = () => {
+  return (
+    <button
+      className='btn'
+      id='main'
+      onClick={() => {alert('Hello')}}
+      disabled
+    >
+      Click me
+    </button>
+  )
+}
+
